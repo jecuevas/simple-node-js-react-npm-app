f9b7727ee6d83c311f1ef2d3ad4897ca72894701
Jhonathan Cuevas <jhonatan.cuevas.o@gmail.com>
2019-11-09 15:54:36 -0500

    imprimiendo variable

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {value: 'hola'}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React, integrate jenkins together to github. cntinuamos aprendiendo</h1>
-          <h2>esto esta quedando excelente, funciono el WebHook, ahora con credenciales</h2>   
+          <h2>esto esta quedando excelente, funciono el WebHook, ahora con credenciales {state.value}</h2>   
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
