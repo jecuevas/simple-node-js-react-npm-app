44948ba417c91b91c645b7d45301bfc2f90a38d4
Jhonathan Cuevas <jhonatan.cuevas.o@gmail.com>
2019-11-09 15:16:03 -0500

    continuamos aprendiendo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React, integrate jenkins together to github</h1>
+          <h1 className="App-title">Welcome to React, integrate jenkins together to github. cntinuamos aprendiendo</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
